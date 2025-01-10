f5b31e505437548341d67e916be97ba6498660d5
Martin George <martingeorge191110@gmail.com>
2025-01-10 14:36:36 +0200

    task 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# create a file in /tmp
+file { '/tmp/school':
+    owner   => 'www-data',
+    mode    => '0744',
+    content => 'I love Puppet',
+    group   => 'www-data',
+    ensure  => 'file',
+}
