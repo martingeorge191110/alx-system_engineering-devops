2b1bfbc164c9717cb5d41258da969a002358a0aa
Martin George <martingeorge191110@gmail.com>
2025-02-10 16:09:49 +0200

    task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
 """Python script that, using this REST API"""
+import csv
 import requests
 from sys import argv
-import json
 
 
 def api_response(api, id):
     user_api = requests.get(f"{api}/users/{id}").json()
     user_tasks_api = requests.get(f"{api}/users/{id}/todos").json()
-    json_format = {f"{id}": []}
+    csv_format = []
     username = user_api['username']
 
-
     for ele in user_tasks_api:
-        json_format[f"{id}"].append(
-            {'task': ele['title'], 'completed': ele['completed'], 'username': username}
-        )
+        csv_format.append([id, username,
+                           ele['completed'], ele['title']
+                           ])
 
-    with open(f"{id}.json", "w", encoding="utf-8") as file:
-        json.dump(json_format, file, indent=4)
+    with open(f"{id}.csv", "w", encoding="utf-8") as file:
+        writer = csv.writer(file, quoting=1)
 
+        for e in csv_format:
+            writer.writerow(e)
 
 
 if __name__ == '__main__':
