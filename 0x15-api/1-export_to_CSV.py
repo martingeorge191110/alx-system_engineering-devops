64c793ab0189267fd360a6bc1cd059cb526f64b7
Martin George <martingeorge191110@gmail.com>
2025-02-10 16:03:45 +0200

    task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Python script that, using this REST API"""
+import requests
+from sys import argv
+import json
+
+
+def api_response(api, id):
+    user_api = requests.get(f"{api}/users/{id}").json()
+    user_tasks_api = requests.get(f"{api}/users/{id}/todos").json()
+    json_format = {f"{id}": []}
+    username = user_api['username']
+
+
+    for ele in user_tasks_api:
+        json_format[f"{id}"].append(
+            {'task': ele['title'], 'completed': ele['completed'], 'username': username}
+        )
+
+    with open(f"{id}.json", "w", encoding="utf-8") as file:
+        json.dump(json_format, file, indent=4)
+
+
+
+if __name__ == '__main__':
+
+    api_response(
+        'https://jsonplaceholder.typicode.com',
+        argv[1]
+        )
