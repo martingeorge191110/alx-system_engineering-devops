17677dde45148ea23aecb91bfce03dabb86c3d6a
Martin George <martingeorge191110@gmail.com>
2025-02-27 15:14:34 +0200

    task 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,7 @@
 # why Apache is returning a 500 error
+
 exec {'fix_apache2':
     provider => shell,
-    command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
+    command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
+    path     => '/usr/local/bin/:/bin/'
 }
